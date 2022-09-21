516b35ce10a7d184a8d3aa76bf56a119f8ea0194
thenewguy321 <aliladha2000@gmail.com>
2022-09-21 12:26:48 -0700

    Finalized Tests and changed behaviour

--- code/avgValue.java
+++ code/avgValue.java
@@ -1,6 +1,8 @@
 package code;
 public class avgValue {
-    public static double avgValue(int[] input){
+    public static double avgValueFinder(int[] input){
+        if (input.length == 0)
+            return 0;
         double sum = 0;
         double avg = 0;
         for (int i = 0;i<input.length;i++)
