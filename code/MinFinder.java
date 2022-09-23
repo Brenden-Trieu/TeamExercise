e5165093279643468266d37df5283f8512bc34f1
Brenden-Trieu <59781947+Brenden-Trieu@users.noreply.github.com>
2022-09-23 12:13:30 -0700

    Revert "Update MinFinder.java"

--- code/MinFinder.java
+++ code/MinFinder.java
@@ -1,5 +1,3 @@
-package code;
-
 
 public class MinFinder {
 	public static void main(String[] args) {
@@ -8,20 +6,19 @@ public class MinFinder {
 		
 		
 	}
-	public static int findMinElem(int[] ar) {
+	static int findMinElem(int[] ar) {
 		int min=ar[0];
 		for(int item:ar)
 			min=Math.min(item, min);
 	
 	return min;
 	}
-	public static String findMinElem(String[] ar) {
+	static String findMinElem(String[] ar) {
 		String s1=ar[0];
 		for(String item:ar) {
 			if(s1.compareTo(item)>0)
 				s1=item;
 		}
 		return s1;
-		//forgot I wasnt supposed to self merge
 	}
 }
