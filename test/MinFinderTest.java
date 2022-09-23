a39eab980146b1b96e1e9f90756310a1122abc1b
Jeena01 <jeenajavahar@gmail.com>
2022-09-23 09:46:02 -0700

    fixed test bug hopefully

--- test/MinFinderTest.java
+++ test/MinFinderTest.java
@@ -7,14 +7,14 @@ class MinFinderTest {
 	@Test
 	void testIntArray() {
 		int[] x= {23,45,12,3,78};
-		assertEquals(MinFinder.findMinElem(x),3,"Integer arrays work");
+		assertEquals(code.MinFinder.findMinElem(x),3,"Integer arrays work");
 		//fail("Not yet implemented");
 		
 	}
 	@Test
 	void testStringArray() {
 		String[] s= {"arc","apple","about"};
-		assertEquals(MinFinder.findMinElem(s),"about","String arrays work");
+		assertEquals(code.MinFinder.findMinElem(s),"about","String arrays work");
 	}
 	
 
